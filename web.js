4dd64b5a2d857fe55d30c86b6753f9d93cffb290
Jp <jp.gallegos.diaz@gmail.com>
2013-07-11 19:27:26 +0000

    Fix web.js bugs

--- web.js
+++ web.js
@@ -2,12 +2,14 @@ var express = require('express');
 var fs = require('fs');
 
 var infile = 'index.html'
-var input = fs.readFileSync(infile).toString('utf-8', 0, input.length);
+var input = fs.readFileSync(infile);
+
+console.log("Read: " + input + "\nFrom: " + infile);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(input);
+  response.send(input.toString('utf-8', 0, input.length);
 });
 
 var port = process.env.PORT || 5000;
