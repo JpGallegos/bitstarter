fd87fb6de04bcab10a81ec828b985c7572059913
Jp <jp.gallegos.diaz@gmail.com>
2013-07-11 19:16:10 +0000

    Edited web.js to read index.html

--- web.js
+++ web.js
@@ -1,9 +1,13 @@
 var express = require('express');
+var fs = require('fs');
+
+var infile = 'index.html'
+var input = fs.readFileSync(infile).toString('utf-8', 0, input.length);
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(input);
 });
 
 var port = process.env.PORT || 5000;
